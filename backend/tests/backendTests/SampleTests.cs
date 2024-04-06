f2369d600fffbd9bc8df8954cb86623ba4ec6765
Elias Büchel <eli.buechel@hotmail.com>
2024-04-06 09:05:17 +0200

    Adding Nunit test project

---
+++ backend/tests/backendTests/SampleTests.cs
@@ -0,0 +1,14 @@
+using temp;
+
+namespace backendTests
+{
+    [TestFixture]
+    public class SampleTests
+    {
+        [Test]
+        public void Sample_BaseOperation_True()
+        {
+            Assert.True(true);
+        }
+    }
+}
