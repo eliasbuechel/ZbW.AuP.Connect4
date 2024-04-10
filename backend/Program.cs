7792e6abea563663faef78dbed36f83946c64b5e
Elias Büchel <eli.buechel@hotmail.com>
2024-04-10 10:36:12 +0200

    Creating DbContext with DbContextFactory and DesignTimeDbConextFactory

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,14 +1,45 @@
-namespace temp
+using backend.database;
+using Microsoft.EntityFrameworkCore;
+
+namespace backend
 {
     public class Program
     {
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
-            var app = builder.Build();
+
+            builder.Services.AddSingleton(s => new BackendDbContextFacory(new DbContextOptionsBuilder().UseMySQL("Server=127.0.0.1;Port=3307;Database=R4D4_Dev;Uid=EntityFW;Pwd=PwEF54762-@R4D4lul;").Options));
+
+            WebApplication app = builder.Build();
+
+            {
+                BackendDbContextFacory dbContextFactory = app.Services.GetRequiredService<BackendDbContextFacory>();
+                using BackendDbContext context = dbContextFactory.GetDbContext();
+                try
+                {
+                    context.Database.Migrate();
+                }
+                catch (Exception e)
+                {
+                    Logger.Log(LogCase.ERROR, "Not able to migrate database", e);
+                    return;
+                }
+            }
+
+            {
+                BackendDbContextFacory dbContextFactory = app.Services.GetRequiredService<BackendDbContextFacory>();
+                using BackendDbContext context = dbContextFactory.GetDbContext();
+
+                context.Users.Add(new User { Id = 1 });
+                context.SaveChanges();
+
+                User[] users = context.Users.ToArray();
+                foreach (var user in users)
+                    Console.WriteLine($" user<{user.Id}>");
+            }
 
             app.MapGet("/", () => "Hello World!");
-
             app.Run();
         }
     }
---
+++ backend/database/BackendDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace backend.database
+{
+    internal class BackendDbContext : DbContext
+    {
+        public BackendDbContext(DbContextOptions options) : base(options)
+        { }
+
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.LogTo(message => Logger.Log(LogCase.DEBUG, $"ENTITY_FRAMEWORK: {message}"));
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
