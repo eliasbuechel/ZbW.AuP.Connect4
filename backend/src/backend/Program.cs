e78e5a9261bf6060c79732aa6e13f2dd350a1fdb
Elias Büchel <eli.buechel@hotmail.com>
2024-04-10 16:29:55 +0200

    Configure MqttClient via the config file

--- backend/src/backend/Program.cs
+++ backend/src/backend/Program.cs
@@ -8,8 +8,21 @@ namespace backend
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+            builder.Configuration.AddJsonFile("appsettings.json");
 
-            builder.Services.AddSingleton(s => new MqttTopicClient(IPAddress.Loopback, 1883));
+            IConfiguration config = builder.Configuration;
+            string environment = builder.Environment.EnvironmentName;
+
+            {
+                IConfiguration mqttConfig = config.GetSection($"{environment}:MqttClient");
+                string? mqttIpAddressString = mqttConfig["IpAddress"];
+                if (string.IsNullOrEmpty(mqttIpAddressString))
+                    throw new Exception("Invalid MQTT client IP address configuration.");
+                IPAddress mqttIpAddress = IPAddress.Parse(mqttIpAddressString);
+                int mqttPort = mqttConfig.GetValue<int>("Port");
+
+                builder.Services.AddSingleton(s => new MqttTopicClient(mqttIpAddress, mqttPort));
+            }
 
             var app = builder.Build();
 
