27ef76c6290a9b6f1a213017b03607e73c79b849
Elias Büchel <eli.buechel@hotmail.com>
2024-04-14 13:25:00 +0200

    Restructure to use Startup.cs

--- backend/src/backend/Program.cs
+++ backend/src/backend/Program.cs
@@ -1,4 +1,5 @@
 using backend.database;
+using Google.Protobuf.WellKnownTypes;
 using Microsoft.EntityFrameworkCore;
 
 namespace backend
@@ -8,6 +9,24 @@ namespace backend
         public static void Main(string[] args)
         {
             IHost webAppHost = CreateHostBuilder(args).Build();
+
+            try
+            {
+                BackendDbContextFacory dbContextFactory = webAppHost.Services.GetRequiredService<BackendDbContextFacory>();
+                using BackendDbContext context = dbContextFactory.GetDbContext();
+
+                context.Users.Add(new User { Id = 1 });
+                context.SaveChanges();
+
+                User[] users = context.Users.ToArray();
+                foreach (var user in users)
+                    Console.WriteLine($" user<{user.Id}>");
+            }
+            catch (Exception e)
+            {
+                Logger.Log(LogCase.ERROR, "Not able to run test on database!", e);
+            }
+
             webAppHost.Run();
         }
         public static IHostBuilder CreateHostBuilder(string[] args)
@@ -18,40 +37,6 @@ namespace backend
                     webBuilder.UseStartup(builder => new Startup(builder.Configuration));
                     webBuilder.UseUrls("http://localhost:7136");
                 });
-            var builder = WebApplication.CreateBuilder(args);
-
-            builder.Services.AddSingleton(s => new BackendDbContextFacory(new DbContextOptionsBuilder().UseMySQL("Server=127.0.0.1;Port=3307;Database=R4D4_Dev;Uid=EntityFW;Pwd=PwEF54762-@R4D4lul;").Options));
-
-            WebApplication app = builder.Build();
-
-            {
-                BackendDbContextFacory dbContextFactory = app.Services.GetRequiredService<BackendDbContextFacory>();
-                using BackendDbContext context = dbContextFactory.GetDbContext();
-                try
-                {
-                    context.Database.Migrate();
-                }
-                catch (Exception e)
-                {
-                    Logger.Log(LogCase.ERROR, "Not able to migrate database", e);
-                    return;
-                }
-            }
-
-            {
-                BackendDbContextFacory dbContextFactory = app.Services.GetRequiredService<BackendDbContextFacory>();
-                using BackendDbContext context = dbContextFactory.GetDbContext();
-
-                context.Users.Add(new User { Id = 1 });
-                context.SaveChanges();
-
-                User[] users = context.Users.ToArray();
-                foreach (var user in users)
-                    Console.WriteLine($" user<{user.Id}>");
-            }
-
-            app.MapGet("/", () => "Hello World!");
-            app.Run();
         }
     }
 }
--- backend/src/backend/Startup.cs
+++ backend/src/backend/Startup.cs
@@ -1,4 +1,7 @@
-﻿namespace backend
+﻿using backend.database;
+using Microsoft.EntityFrameworkCore;
+
+namespace backend
 {
     public class Startup
     {
@@ -11,9 +14,23 @@
 
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton(s => new BackendDbContextFacory(new DbContextOptionsBuilder().UseMySQL("Server=127.0.0.1;Port=3307;Database=R4D4_Dev;Uid=EntityFW;Pwd=PwEF54762-@R4D4lul;").Options));
         }
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            {
+                BackendDbContextFacory dbContextFactory = app.ApplicationServices.GetRequiredService<BackendDbContextFacory>();
+                using BackendDbContext context = dbContextFactory.GetDbContext();
+                try
+                {
+                    context.Database.Migrate();
+                }
+                catch (Exception e)
+                {
+                    Logger.Log(LogCase.ERROR, "Not able to migrate database", e);
+                    return;
+                }
+            }
         }
     }
 }
