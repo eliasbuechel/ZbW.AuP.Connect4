e0259c083f9628216d6570b2fdf5510fb9a6d0a4
Elias Büchel <eli.buechel@hotmail.com>
2024-04-16 20:13:58 +0200

    Updating connection string for local database

--- backend/src/backend/database/BackendDesignTimeDbContextFactory.cs
+++ backend/src/backend/database/BackendDesignTimeDbContextFactory.cs
@@ -7,7 +7,7 @@ namespace backend.database
     {
         public BackendDbContext CreateDbContext(string[] args)
         {
-            const string connectionString = "Server=127.0.0.1;Port=3307;Database=R4D4_Dev;Uid=EntityFW;Pwd=PwEF54762-@R4D4lul;";
+            const string connectionString = "Server=127.0.0.1;Port=3306;Database=r4d4_loc;Uid=root;Pwd=root;";
 
             DbContextOptions options = new DbContextOptionsBuilder().UseMySQL(connectionString).Options;
             return new BackendDbContext(options);
