72a36677dd592dde13b4c6c4e088b212a3166e14
GaborElAyoubi <gabor_ela.hotmail.com@zbw-online.ch>
2024-04-25 19:22:14 +0200

    adding Guard for Routings

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,29 +1,23 @@
-// Externe Bibliotheken/Frameworks
+// extern libraris/frameworks
 import { Router, createRouter, createWebHistory } from "vue-router";
 import axios, { AxiosStatic } from "axios";
 
-// Interne Module
+// Intern modules
 import { App, createApp } from "vue";
 import AppVue from "./App.vue";
+import routes from "@/routes";
 
-// Lokale Dateien/Komponenten
-import HomeView from "@/views/HomeView.vue";
-import Login from "@/components/LoginForm.vue";
-import Register from "@/components/RegisterForm.vue";
-import GameView from "./views/GameView.vue";
-import EmailVerificationForm from "./components/EmailVerificationForm.vue";
-
-const router: Router = createRouter({
+const router = createRouter({
   history: createWebHistory(),
-  routes: [
-    { path: "/", name: "Home", component: HomeView },
-    { path: "/login", name: "Login", component: Login },
-    { path: "/register", name: "Register", component: Register },
-    { path: "/game", name: "Game", component: GameView },
-    { path: "/email-verification", name: "EmailVerificationForm", component: EmailVerificationForm}
-  ],
+  routes
 });
 
+export default router;
+
+//Guard-API to controll access to URL's
+
+
+// Register (main)App
 const app: App = createApp(AppVue);
 
 declare module "@vue/runtime-core" {
@@ -36,3 +30,4 @@ app.config.globalProperties.$axios = axios;
 
 app.use(router);
 app.mount("#app");
+
