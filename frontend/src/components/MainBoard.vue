9e6afe4de0aba14db0086a6f1d0c4dc2a7fda185
Elias Büchel <eli.buechel@hotmail.com>
2024-05-27 16:24:29 +0200

    Improving styling of the frontend

--- frontend/src/components/MainBoard.vue
+++ frontend/src/components/MainBoard.vue
@@ -1,11 +1,13 @@
 <template>
-  <div class="grid-container">
-    <img id="logo-home" src="@/assets/images/Logo.png" alt="r4d4-logo" class="r4d4-logo grid-item-page-info" />
-    <UserInfo :identity="identity" class="grid-item-user-info" />
-    <BestList :bestlist="bestlist" @show-replay="showReplay" class="grid-item-best-list" />
-    <SinglePlayerModeSelection class="grid-item-signle-player-mode-selection" />
-    <OnlinePlayersListing :onlinePlayers="onlinePlayers" :identity="identity" class="grid-item-online-player-listing" />
-    <GamePlan class="grid-item-game-plan" :gamePlan="gamePlan" />
+  <div class="main-board-container">
+    <div class="header-container">
+      <img id="logo-home" src="@/assets/images/Logo.png" alt="r4d4-logo" class="r4d4-logo" />
+      <UserInfo :identity="identity" />
+    </div>
+    <SinglePlayerModeSelection class="content-card" />
+    <OnlinePlayersListing :onlinePlayers="onlinePlayers" :identity="identity" class="content-card" />
+    <GamePlan class="content-card" :gamePlan="gamePlan" />
+    <BestList :bestlist="bestlist" @show-replay="showReplay" class="content-card" />
   </div>
 </template>
 
@@ -64,31 +66,3 @@ export default defineComponent({
   },
 });
 </script>
-
-<style>
-.grid-item-page-info {
-  grid-column: 1 / span 3;
-  grid-row: 1 / span 2;
-}
-.grid-item-user-info {
-  grid-column: 10 / span 3;
-  grid-row: 1 / span 2;
-}
-.grid-item-signle-player-mode-selection {
-  grid-column: 7 / span 6;
-  grid-row: 3 / span 5;
-}
-.grid-item-best-list {
-  grid-column: 1 / span 6;
-  grid-row: 8 / span 5;
-}
-.grid-item-online-player-listing {
-  grid-column: 1 / span 6;
-  grid-row: 3 / span 5;
-}
-.grid-item-game-plan {
-  grid-column: 7 / span 6;
-  grid-row: 8 / span 5;
-}
-</style>
-./BestList.vue@/types/GameResultMatch
