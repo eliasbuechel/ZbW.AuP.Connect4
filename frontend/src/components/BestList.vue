d66b089f38e858addea7a7248522b996c02e61ad
GaborElAyoubi <gabor_ela.hotmail.com@zbw-online.ch>
2024-06-18 20:20:27 +0200

    Bestlist search

--- frontend/src/components/BestList.vue
+++ frontend/src/components/BestList.vue
@@ -2,12 +2,19 @@
   <div class="container">
     <div class="listing-container">
       <h2>Bestlist</h2>
+      <input class="bestlist-search" v-model="searchTerm" placeholder="Search..." @blur="clearSearch" />
       <ul>
-        <li v-for="(gameResult, idx) in bestlist" :key="gameResult.id" class="game-result-entrie">
-          <span :class="{ winner: !isDraw(idx), draw: isDraw(idx) }">{{ winner(idx).username }}</span>
+        <li v-for="gameResult in filteredBestlist" :key="gameResult.id" class="game-result-entrie">
+          <span
+            :class="{ winner: !checkIfGameHasWinningRow(gameResult), draw: checkIfGameHasWinningRow(gameResult) }"
+            >{{ showWinner(gameResult).username }}</span
+          >
           <span> vs. </span>
-          <span :class="{ loser: !isDraw(idx), draw: isDraw(idx) }">{{ loser(idx).username }}</span>
-          <button class="button-light" @click="showReplay(idx)">Replay</button>
+          <span
+            :class="{ loser: !checkIfGameHasWinningRow(gameResult), draw: checkIfGameHasWinningRow(gameResult) }"
+            >{{ ShowLoser(gameResult).username }}</span
+          >
+          <button class="button-light" @click="showReplay(gameResult)">Replay</button>
         </li>
       </ul>
     </div>
@@ -15,57 +22,84 @@
 </template>
 
 <script lang="ts">
-import { GameResult } from "@/types/GameResult";
-import { PlayerIdentity } from "@/types/PlayerIdentity";
-import { PropType, defineComponent } from "vue";
+  import { GameResult } from "@/types/GameResult";
+  import { PlayerIdentity } from "@/types/PlayerIdentity";
+  import { PropType, defineComponent } from "vue";
 
-export default defineComponent({
-  name: "BestList",
-  props: {
-    bestlist: {
-      required: true,
-      type: Array as PropType<GameResult[]>,
+  export default defineComponent({
+    name: "BestList",
+    props: {
+      bestlist: {
+        required: true,
+        type: Array as PropType<GameResult[]>,
+      },
     },
-  },
-  emits: ["show-replay"],
-  methods: {
-    showReplay(idx: number): void {
-      this.$emit("show-replay", this.bestlist[idx]);
+    data(): { searchTerm: string } {
+      return {
+        searchTerm: "",
+      };
     },
-    winner(idx: number): PlayerIdentity {
-      let gameResult: GameResult = this.bestlist[idx];
-      if (this.isDraw(idx)) return gameResult.match.player1;
-
-      return gameResult.winnerId === gameResult.match.player1.id ? gameResult.match.player1 : gameResult.match.player2;
+    emits: ["show-replay"],
+    methods: {
+      showReplay(gameResult: GameResult): void {
+        let idx = this.bestlist.indexOf(gameResult);
+        this.$emit("show-replay", this.bestlist[idx]);
+      },
+      showWinner(gameResult: GameResult): PlayerIdentity {
+        if (this.checkIfGameHasWinningRow(gameResult)) return gameResult.match.player1;
+        return gameResult.winnerId === gameResult.match.player1.id
+          ? gameResult.match.player1
+          : gameResult.match.player2;
+      },
+      ShowLoser(gameResult: GameResult): PlayerIdentity {
+        return this.showWinner(gameResult).id === gameResult.match.player1.id
+          ? gameResult.match.player2
+          : gameResult.match.player1;
+      },
+      checkIfGameHasWinningRow(gameResult: GameResult): boolean {
+        return gameResult.winnerId === null;
+      },
+      clearSearch() {
+        // Timeout to prevent clearing the search term before the click event on the replay button is triggered
+        setTimeout(() => {
+          this.searchTerm = "";
+        }, 1000);
+      },
     },
-    loser(idx: number): PlayerIdentity {
-      let gameResult: GameResult = this.bestlist[idx];
-      return this.winner(idx).id === gameResult.match.player1.id ? gameResult.match.player2 : gameResult.match.player1;
+    computed: {
+      filteredBestlist(): GameResult[] {
+        if (this.searchTerm === "") return this.bestlist;
+        return this.bestlist.filter(
+          (gameResult) =>
+            gameResult.match.player1.username.toLowerCase().includes(this.searchTerm.toLowerCase()) ||
+            gameResult.match.player2.username.toLowerCase().includes(this.searchTerm.toLowerCase())
+        );
+      },
     },
-    isDraw(idx: number): boolean {
-      //   let gameResult: GameResult = this.bestlist[idx];
-      return this.bestlist[idx].winnerId == null;
-    },
-  },
-});
+  });
 </script>
 
 <style scoped>
-.winner {
-  font-weight: bolder;
-}
-
-.game-result-entrie {
-  display: flex;
-  align-items: center;
-  flex-direction: row;
-}
-
-.game-result-entrie>button {
-  margin-left: auto;
-}
-
-.game-result-entrie>span {
-  margin-right: 0.5rem;
-}
+  .bestlist-search {
+    padding: 0.2rem 0.8rem;
+    background-color: transparent;
+    border: 2px solid var(--color-orange);
+    border-radius: 0.5em;
+    font-size: 1rem;
+    color: var(--color-light);
+  }
+  .winner {
+    font-weight: bolder;
+  }
+  .game-result-entrie {
+    display: flex;
+    align-items: center;
+    flex-direction: row;
+  }
+  .game-result-entrie > button {
+    margin-left: auto;
+  }
+  .game-result-entrie > span {
+    margin-right: 0.5rem;
+  }
 </style>
