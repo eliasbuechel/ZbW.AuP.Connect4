83bcd21b8cd0a7970c82ec62f355c3878856eac8
Elias Büchel <eli.buechel@hotmail.com>
2024-07-02 16:21:20 +0200

    Increacing timout time

--- backend/src/backend/communication/mqtt/RoboterAPI.cs
+++ backend/src/backend/communication/mqtt/RoboterAPI.cs
@@ -120,7 +120,7 @@ namespace backend.communication.mqtt
         private Guid _resetRequestId = Guid.Empty;
         private Guid _placeStoneRequestId = Guid.Empty;
         private const int TESTING_REQUEST_TIMOUT_TIME_IN_MS = 3000; // 3s
-        private const int ROBOTER_MOVE_REQUEST_TIMOUT_TIME_IN_MS = 30000; // 30s
-        private const int ROBOTER_RESET_REQUEST_TIMOUT_TIME_IN_MS = 60000; // 60s
+        private const int ROBOTER_MOVE_REQUEST_TIMOUT_TIME_IN_MS = 3000000; // 30s
+        private const int ROBOTER_RESET_REQUEST_TIMOUT_TIME_IN_MS = 6000000; // 60s
     }
 }
